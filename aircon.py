ad95abf39c50a44c4380c40d28344d65228e956e
nakano57 <hiroto856@gmail.com>
2019-08-07 01:34:48 +0900

    add change temperature

--- aircon.py
+++ aircon.py
@@ -1,17 +1,25 @@
 import sys
 import json
 import os
+import subprocess
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
+import math
+
+
+def my_round(x, d=0):
+    p = 10 ** d
+    return int(float(math.floor((x * p) + math.copysign(0.5, x)))/p)
 
 
 class ac_get:
     def __init__(self, file):
         self.status = {
-            'CurrentTemperature': 0,
+            'CurrentTemperature': 30,
             # Minimum Value 0
             # Maximum Value 100
             # Step Value 0.1
 
-            'SwingMode': 1,
+            'SwingMode': 0,
             # 0 - "Swing disabled"
             # 1 - "Swing enabled"
 
@@ -21,35 +29,35 @@ class ac_get:
             # Step Value: 1
             # Unit: percentage
 
-            'TargetHeaterCoolerState': 3,
+            'TargetHeaterCoolerState': 2,
             # Valid Values
             # 0 - AUTO
             # 1 - HEAT
             # 2 - COOL
 
-            'LockPhysicalControls': 4,
+            'LockPhysicalControls': 0,
             # 0 - "Control lock disabled"
             # 1 - "Control lock enabled"
 
-            'HeatingThresholdTemperature': 5,
+            'HeatingThresholdTemperature': 25,
             # Minimum Value: 0
             # Maximum Value: 25
             # Step Value: 0.1
             # Unit: celcius
 
-            'CurrentHeaterCoolerState': 'COOLING',
+            'CurrentHeaterCoolerState': 3,
             # 0 - INACTIVE
             # 1 - IDLE
             # 2 - HEATING
             # 3 - COOLING
 
-            'CoolingThresholdTemperature': 7,
+            'CoolingThresholdTemperature': 26,
             # Minimum Value: 10
             # Maximum Value: 35
             # Step Value: 0.1
             # Unit: celcius
 
-            'Active': 8,
+            'Active': 1,
             # 0 - "Inactive"
             # 1 - "Active"
 
@@ -76,23 +84,52 @@ class ac_get:
             raise ValueError('Invalid Value: {}'.format(args))
 
 
+class cool_set(ac_get):
+    def __init__(self, file):
+        super(cool_set, self).__init__(file)
+        self.__STATUS = 'cool'
+
+    def SetValue(self, arg3, arg4):
+        if arg3 == 'CoolingThresholdTemperature':
+            self.ChangeTenperature(arg4)
+
+    def ChangeTenperature(self, temp):
+        rounded = my_round(float(temp))
+        cmd = 'python3 ~/pigpio/irrp2.py -p -g17 -f ~/pigpio/aircon_{0} ac:{0}_{1}'.format(self.__STATUS,
+                                                                                           rounded)
+        res = subprocess.run(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # print(f'{i},{res.stdout.decode("utf8")}')
+        self.status['CoolingThresholdTemperature'] = rounded
+        print(rounded)
+        self.Save(file)
+
+
 if __name__ == "__main__":
+    with open('./test.txt', 'r+') as file:
+        f = open('./test.txt', "a")
 
-    # if os.path.exists("./acVariableHolder") == False:
-    #     f = open('./acVariableHolder', 'w')
-    #     f.close()
+        for i in sys.argv:
+            text = i + ' '
+            f.write(text)
 
-        # with open('/tmp/acVariableHolder', 'r+') as file:
-    with open('./acVariableHolder', 'w+') as file:
+        f.write('\n')
 
-        # if len(sys.argv) != 4:
+    with open('/tmp/acVariableHolder', 'w+') as file:
+        # with open('./acVariableHolder', 'w+') as file:
+
+        # if len(sys.argv) != 5:
         #    sys.exit(1)
 
         if sys.argv[1] == 'Get':
             get = ac_get(file)
-            get.getValue(sys.argv[3])
-            get.Save(file)
             print(get.getValue(sys.argv[3]))
-            sys.exit(get.getValue(sys.argv[3]))
+            get.Save(file)
+            sys.exit(0)
+
+        if sys.argv[1] == 'Set':
+            set = cool_set(file)
+            set.SetValue(sys.argv[3], sys.argv[4])
+            sys.exit()
 
         sys.exit()
